5f218c1f3a0955d1a38123552f4d1719241c3ef4
Arpit Soni <151952369+Arpitsoni89@users.noreply.github.com>
2026-07-16 00:00:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="greet-package-updated",
+    name="greet_package_updated",
     version="0.0.1",
     description="A simple greeting package",
     author="Arpit Soni",
