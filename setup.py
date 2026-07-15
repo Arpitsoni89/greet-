3a86dfd110a51196cf58d6aa72a55aa7b03770c9
Arpit Soni <151952369+Arpitsoni89@users.noreply.github.com>
2026-07-15 17:46:18 +0530

    Create setup.py for sayhi1-greet package
    Add setup.py for packaging the greeting module.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sayhi1-greet",
+    version="0.1.0",
+    description="A simple greeting package",
+    author="Arpit Soni",
+    author_email="your-email@example.com",
+    url="https://github.com/Arpitsoni89/greet-",
+    py_modules=["sayhi1_greet"],
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
