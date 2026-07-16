6072a9a967c7925253cfd9e3abc130e41ecdac3a
Arpit Soni <151952369+Arpitsoni89@users.noreply.github.com>
2026-07-16 13:41:24 +0530

    Add greeting functions in test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+"""
+A simple greeting package
+"""
+
+def greet():
+    return "Hello! Welcome to SayHi Greet!"
+
+def hello(name="User"):
+    return f"Hello, {name}!"
+
+def goodbye(name="User"):
+    return f"Goodbye, {name}! See you soon!"
+
+def greet_formal():
+    return "Greetings and salutations!"
+
+
